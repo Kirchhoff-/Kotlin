c74acbf3a1d7e315a306ed37cbd9248baa9ca8eb
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-05-03 22:56:22 +0300

    Add Kotlin Android Extensions.(Chapter 15)

--- app/src/main/java/com/example/kirchhoff/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kirchhoff/kotlin/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.kirchhoff.kotlin
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.async
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
@@ -13,8 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
         //Also can find and access RecyclerView using Anko
         //val forecastList: RecyclerView = find(R.id.forecast_list)
 
