ee66c4e02d724ef81f54ac48c2386e5ad0b0f6c2
Kirchhoff- <kirchhoff.19.92@gmail.com>
2019-08-28 18:09:43 +0300

    Added few coroutines examples.

--- coroutines/src/main/java/com/example/coroutines/CoroutinesActivity.kt
+++ coroutines/src/main/java/com/example/coroutines/CoroutinesActivity.kt
@@ -3,18 +3,17 @@ package com.example.coroutines
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
-class CoroutinesActivity: AppCompatActivity() {
+class CoroutinesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutines)
 
-        findViewById<View>(R.id.bFirstExample).setOnClickListener{ fistExample() }
+        findViewById<View>(R.id.bFirstExample).setOnClickListener { fistExample() }
+        findViewById<View>(R.id.bSecondExample).setOnClickListener { secondExample() }
+        findViewById<View>(R.id.bThirdExample).setOnClickListener { thirdExample() }
     }
 
     private fun fistExample() {
@@ -42,5 +41,49 @@ class CoroutinesActivity: AppCompatActivity() {
         }
 
         println("Hello,")
+    }
+
+    private fun secondExample() {
+        runBlocking {
+            launch {
+                delay(200L)
+                println("Task from runBlocking")
+            }
+
+            coroutineScope {
+                // Creates a new coroutine scope
+                val job = launch {
+                    println("Task from nested launch, this is printed")
+                    delay(500L)
+                    println("Task from nested launch, this won't be printed")
+                }
+
+                delay(100L)
+                println("Task from first coroutine scope") // Printed before initial launch
+                job.cancel() // This cancels nested launch's execution
+            }
+
+            println("Coroutine scope is over") // This is not printed until nested launch completes/is cancelled
+        }
+    }
+
+    private fun thirdExample() {
+        runBlocking {
+            val startTime = System.currentTimeMillis()
+            val job = launch(Dispatchers.Default) {
+                var nextPrintTime = startTime
+                var i = 0
+                while (isActive) {
+                    if (System.currentTimeMillis() >= nextPrintTime) {
+                        println("I'm sleeping ${i++} ...")
+                        nextPrintTime += 500L
+                    }
+                }
+            }
+            delay(1300L) // delay a bit
+            println("main: I'm tired of waiting!")
+            job.cancelAndJoin() // cancels the job and waits for its completion
+            println("main: Now I can quit.")
+        }
     }
 }
