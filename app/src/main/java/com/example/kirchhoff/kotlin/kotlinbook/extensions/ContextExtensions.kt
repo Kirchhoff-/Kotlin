7d5aa710b629a02a9ce4976742e949fe26f4b020
Kirchhoff- <kirchhoff.19.92@gmail.com>
2022-06-01 11:18:34 +0300

    Issue-14: Removed kotlinbook package and related libraries.

--- app/src/main/java/com/example/kirchhoff/kotlin/kotlinbook/extensions/ContextExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kirchhoff.kotlin.kotlinbook.extensions
-
-import android.content.Context
-import android.support.v4.content.ContextCompat
-
-/**
- * @author Kirchhoff-
- */
-fun Context.color(res: Int): Int = ContextCompat.getColor(this, res)
